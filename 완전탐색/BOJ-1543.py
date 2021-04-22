7c0a6010fa8e1b5163dad9b7e55ffad063d8f875
Danna <songda515@gmail.com>
2021-04-22 18:00:49 +0900

    [완전탐색] BOJ 1543 문서 검색 - 두 번째 풀이

--- 완전탐색/BOJ-1543.py
+++ 완전탐색/BOJ-1543.py
@@ -7,17 +7,5 @@ input = stdin.readline
 
 document = input().strip()
 word = input().strip()
-n = len(word)
-i = 0
-count = 0
 
-while i < len(document):
-    print(i, i+n, document[i:i+n] == word, document[i:i+n], count)
-
-    if document[i:i+n] == word:
-        count += 1
-        i += n
-    else:
-        i += 1
-
-print(count)
+print(document.count(word))
