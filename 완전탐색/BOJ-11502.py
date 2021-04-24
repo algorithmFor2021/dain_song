c1dd2d9f44f35594e34e9aa1049c932cd96241b9
Danna <songda515@gmail.com>
2021-04-24 12:26:42 +0900

    [완전탐색] BOJ 11502 세 개의 소수 문제 - 두 번째 풀이

--- 완전탐색/BOJ-11502.py
+++ 완전탐색/BOJ-11502.py
@@ -13,32 +13,20 @@ def get_primes(n):
                 a[j] = False
     return primes
 
-def find_primes(total, count, primes):
-    global target, result
-
-    if total > target:
-        return False
-    if count == 3:
-        if total == target:
-            return True
-        return False
-
-    for p in primes:
-        if find_primes(total + p, count + 1, primes):
-            result.append(p)
-            return True
+def find_primes(target, primes):
+    for x in primes:
+        total = 0
+        for y in primes:
+            for z in primes:
+                total = x + y + z
+                if total == target:
+                    print(x, y, z)
+                    return
 
 T = int(input())
 primes = get_primes(1000)
 
 for t in range(T):
     target = int(input())
-    result = []
     filtered_primes = list(filter(lambda x: x<target, primes))
-    for prime in filtered_primes:
-        if find_primes(prime, 1, filtered_primes):
-            result.append(prime)
-            break
-
-    result.sort()
-    print(' '.join(map(str, result)))
+    find_primes(target, filtered_primes)
