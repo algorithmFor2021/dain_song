ed1d7c47b71c36bee0ea44f9c44d633884f3f32a
Danna <songda515@gmail.com>
2021-04-27 00:25:59 +0900

    [완전탐색] BOJ 1405 미친로봇 - 두 번째 풀이

--- 완전탐색/BOJ-1405.py
+++ 완전탐색/BOJ-1405.py
@@ -2,37 +2,25 @@
 # 미친로봇
 
 # E 동 W 서 N 북 S 남
-directions = (
-    (0, 1, 1, 0, 0, 0),
-    (0, -1, 0, 1, 0, 0),
-    (-1, 0, 0, 0, 1, 0), 
-    (1, 0, 0, 0, 0, 1))
-
-def dfs(num, y, x, e, w, n, s):
-    global total, probs
-    if x < 0 or x > 29 or y < 0 or y > 29:
-        return False
-
-    if num == 0:
-        p = pow(probs[0], e) * pow(probs[1], w) * pow(probs[2], n) * pow(probs[3], s)
-        total += p
-        return True
-
-    visited[y][x] = True
-    for d in directions:
-        ny, nx = y+d[0], x+d[1]
-        if visited[ny][nx]:
-            continue
-        dfs(num-1, ny, nx, e+d[2], w+d[3], n+d[4], s+d[5])
-    visited[y][x] = False
-
-
-visited = []
-for _ in range(29):
-    visited.append([False] * 29)
-
+dy = [0, 0, -1, 1]
+dx = [1, -1, 0, 0]
+visited = [[False] * 29 for _ in range(29)]
 num, E, W, N, S = map(int, input().split())
 probs = [E/100, W/100, N/100, S/100]
 total = 0
-dfs(num, 14, 14, 0, 0, 0, 0)
+
+def dfs(num, y, x, p):
+    global total, probs
+    if num == 0:
+        total += p
+        return
+
+    visited[y][x] = True
+    for i in range(4):
+        ny, nx = y+dy[i], x+dx[i]
+        if not visited[ny][nx]:
+            dfs(num-1, ny, nx, p * probs[i])
+    visited[y][x] = False
+
+dfs(num, 14, 14, 1)
 print(total)
