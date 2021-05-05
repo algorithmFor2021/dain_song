947457afae31305376d6e63efe0afb583a6b09ab
Danna <songda515@gmail.com>
2021-05-06 01:19:58 +0900

    [그리디] BOJ 2529 부등호 - 두번째풀이(참고)

--- 그리디/BOJ-2529.py
+++ 그리디/BOJ-2529.py
@@ -3,32 +3,39 @@
 
 k = int(input())
 marks = input().split()
+marks.append(' ') # 부등호개수 < 자릿수이므로 한개추가
 
-nums = [False] * 10
-min_num = '9876543210'
-max_num = '0123456789'
-
-def dfs(i, nums, num, total):
-    global min_num, max_num, marks, k
-
-    if i == k:
-        min_num = min(min_num, total)
-        max_num = max(max_num, total)
-        return
-    
-    nums[num] = True
-    for n in range(10):
-        # 아직 선택되지 않은 숫자인 경우에만 진행
-        if nums[n]:
-            continue
-        if marks[i] == '<' and num < n:
-            dfs(i+1, nums, n, total + str(n))            
-        elif marks[i] == '>' and num > n:
-            dfs(i+1, nums, n, total + str(n))
-    nums[num] = False
-
-for n in range(10):
-    dfs(0, nums, n, str(n))
+# 최대값
+count, MAX = 0, 9
+max_num = ''
+for i in range(k+1):
+    # < 부등호가 나온 경우에는 가능한 최대수보다 더 작은 수를 앞에 배치해야함
+    # 7 < 8 < 9 ...
+    if marks[i] == '<':
+        count += 1
+    else:
+        next_max = MAX - count - 1
+        # 연속된 < 개수보다 작은 수를 앞에 배치
+        while count > 0:
+            max_num += str(MAX - count)
+            count -= 1
+        max_num += str(MAX) # 현재 가장 큰 수를 배치
+        MAX = next_max # 가능한 최대수를 변경
 
 print(max_num)
+
+# 최소값
+count, MIN = 0, 0
+min_num = ''
+for i in range(k+1):
+    if marks[i] == '>':
+        count += 1
+    else:
+        next_min = MIN + count + 1
+        while count > 0:
+            min_num += str(MIN + count)
+            count -= 1
+        min_num += str(MIN)
+        MIN = next_min
+
 print(min_num)
