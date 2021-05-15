11cc5db17e519446db6cd47092cd231acfc5bc29
Danna <songda515@gmail.com>
2021-05-15 22:08:40 +0900

    [DP] BOJ 2698 인접한 비트의 개수

---
+++ DP/BOJ-2698.py
@@ -0,0 +1,22 @@
+# BOJ-2698.py
+# 인접한 비트의 개수
+# n개의 수열, 인접한 비트의 개수 k < n 
+
+N = 101
+series = [[[0, 0] for k in range(N)] for n in range(N)]
+series[1][0][0] = 1 # n=1, k=0, 마지막자리 0 -> 하나
+series[1][0][1] = 1 # n=1, k=0, 마지막자리 1 -> 하나
+
+for n in range(2, N):
+    for k in range(n):
+        series[n][k][0] = series[n-1][k][0] + series[n-1][k][1]
+        if k > 0:
+            series[n][k][1] = series[n-1][k][0] + series[n-1][k-1][1]
+        else:
+            series[n][k][1] = series[n-1][k][0]
+
+
+n = int(input())
+for _ in range(n):
+    n, k = map(int, input().split())
+    print(series[n][k][0] + series[n][k][1])
